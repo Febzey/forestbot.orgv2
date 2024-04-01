ee3d32b6cdd97d2d19703f68ab7dd2842325cfb9
Febzey <braydent1235@gmail.com>
2024-04-01 02:29:23 -0500

    Fix console.log statements in graphBuilder.tsx

--- src/app/s/[server]/components/graphs/graphBuilder.tsx
+++ src/app/s/[server]/components/graphs/graphBuilder.tsx
@@ -9,33 +9,14 @@ async function getGraphData(server: string) {
 
     const d = await data.json();
 
-<<<<<<< HEAD
-    console.log(d," dataaa")
-=======
-    console.log(d, "data from getGraphData")
->>>>>>> df28543f33c825b5e6934e355640194212c271d5
-
-
     return d;
 
 }
 
 export default async function GraphBuilder({ server }: { server: string} ) {
 
-<<<<<<< HEAD
     let serverData:PlayerActivityHourlyResult[]|any | null | undefined = await getGraphData(server);
-=======
-    const serverData:PlayerActivityHourlyResult[]|any | null | undefined = await getGraphData(server);
-    //make a 5 sec timeout
-    // do it without commenting for fuck sakes
-    // const sleep = (time:number) => new Promise(res=>setTimeout(res,time))
 
-    // await sleep(5000)
-   // console.log("Received data:", JSON.stringify(serverData));
-    
-   console.log(serverData, " server data")
->>>>>>> df28543f33c825b5e6934e355640194212c271d5
-    
     if ("player_activity_by_hour" in serverData) {
         serverData = serverData.player_activity_by_hour;
     }
