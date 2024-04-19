3d09ee6ad173791b8584fb79d483cedaac5d2d5b
Febzey <braydent1235@gmail.com>
2024-04-19 15:41:52 -0500

    Update Discord link styling in navbar component

--- src/components/navbar/nav.tsx
+++ src/components/navbar/nav.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
             <div className="flex flex-row justify-between px-10 lg:justify-evenly items-center w-full ">
                 <Link href="/" className="font-Protest text-white text-3xl">ForestBot</Link>
                 <div className="flex flex-row gap-3">
-                    <a href="https://discord.gg/2P8enrdY6t" target="_blank" className="flex hover:text-[#7785cc] bg-[#7785cc] p-2 px-4 rounded-full  text-white  flex-row items-center duration-200 justify-center gap-2">
+                    <a href="https://discord.gg/2P8enrdY6t" target="_blank" className="flex hover:bg-[#a7b1dd] bg-[#7785cc] p-2 px-4 rounded-full  text-white  flex-row items-center duration-200 justify-center gap-2">
                         <FaDiscord className=" text-[2.6rem] " />
                         Join our discord!
                     </a>
