9df78063a0b2d7afd1f34b328057ad9f45a94df2
febzey <braydent1235@gmail.com>
2024-11-27 19:52:04 -0600

    changed api urls.

--- apiGetter.ts
+++ apiGetter.ts
@@ -1,9 +1,9 @@
 import { ForestBotAPI } from "forestbot-api-wrapper-v2";
 
 export const api = new ForestBotAPI({ 
-    apiUrl: "http://127.0.0.1:8000",
+    apiUrl: "http://127.0.0.1:8001",
     logerrors: true,
     apiKey: "123",
-    websocket_url: "ws://127.0.0.1:8000",
+    websocket_url: "ws://127.0.0.1:8001",
     isBotClient: false,
 }); 
